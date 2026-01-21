8526a163c3bb63aa0ab3eb8477f5f5199ad7e8ed
guvensconsultora <guvensconsultora@gmail.com>
2026-01-21 16:57:18 -0300

    Limitar a un almacén determinado

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Inventory Overview: My Default Warehouse",
-    "version": "17.0.1",
+    "version": "19.0.1",
     "category": "Inventory/Inventory",
     "summary": "Filtra el Resumen de inventario por el almacén por defecto del usuario",
     "description": """
